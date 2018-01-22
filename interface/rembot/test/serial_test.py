e4dc04c5977bc63d131f61b178bcb6c92b8f8e5b
Elvis Eshikena <elviseshikena@gmail.com>
2018-01-21 19:13:55 -0800

    Added serial testing template

---
+++ interface/rembot/test/serial_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Testing serial communication between arduio and python
+License is available in LICENSE
+@author eeshiken
+@since 2017-DEC-28
+"""
+
+import logging
+import serial
+
+if __name__ == "__main__":
+    pass
