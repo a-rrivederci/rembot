bdc7df5cd6e153efe36b02992f6f4b1025435593
Elvis Eshikena <elviseshikena@gmail.com>
2018-01-24 23:39:37 -0800

    Updated rembot test

--- firmware/test/rembot_test/rembot_test.ino
+++ firmware/test/rembot_test/rembot_test.ino
@@ -5,64 +5,69 @@
 // @version 2.0.0
 
 // Imports
-// For motor shield V2
 #include <AccelStepper.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 #include <Wire.h>
-// For servos
 #include <Servo.h>
 
+
 // Constants
+// Program defines
 #define VERBOSE 0 // false
 #define VERSION_MAJOR 2
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define BAUD_RATE 9600
+#define FINE_SPEED 1000
+#define MAX_SPEED 1000
+// Hardware defines
+#define BAUD_RATE 115200
+#define ARM_PIN 5
+#define CLAW_PIN 9
+#define INTERRUPT_PIN1 1
+#define INTERRUPT_PIN2 2
+#define INTERRUPT_PIN3 3
+
 
 // Variables
+// Objects
+Servo arm_servo;
+Servo claw_servo;
+
 // Flags
 byte CONNECTED = 0; //false
+byte ARM_IS_RAISED = 0; // false;
+byte CLAW_IS_OPEN = 0; // false;
+
 // Program variables
+double X_ACCEL = 100.0;
+double Y_ACCEL = 100.0;
+int pos = 0;
 char cmd;
 
+// Adafruit MotorShield AFMS bottom - 0 and AFSM top - 1
+Adafruit_MotorShield AFMS0(0x60); // Default address, no jumpers
+Adafruit_MotorShield AFMS1(0x61); // Rightmost jumper closed
 
+// Program methods
 void setup() {
     Serial.begin(BAUD_RATE);
+
     Serial.print("Rembot Uno v");
     Serial.print(VERSION_MAJOR);
     Serial.print(".");
     Serial.print(VERSION_MINOR);
     Serial.print(".");
     Serial.println(VERSION_PATCH);
+
+    // Start boards
+    AFMS0.begin();
+    AFMS1.begin();
+
+    // Attach pins
+    arm_servo.attach(ARM_PIN);
+    claw_servo.attach(CLAW_PIN);
 }
 
 void loop() {
-  switch (CONNECTED) {
-    case 0:
-        if (Serial.available()) {
-            cmd = Serial.read();
-            switch(cmd) {
-                case 'c': 
-                    CONNECTED = 1;
-                    Serial.println('C');
-                    break;
-            }
-        }
-        break;
-    case 1:
-        if (Serial.available()) {
-            cmd = Serial.read();
-            switch(cmd) {
-                case 'k':
-                    count_down();
-                    break;
-                case 'd':
-                    CONNECTED = 0;
-                    Serial.println('D');
-                    break;
-            }
-        }
-        break;
-  } 
 }
