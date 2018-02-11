ca768b4d3ccc9422143ace086177b371c3c1aced
Tony Singh <tonysingh1996@yahoo.com>
2018-02-10 17:28:32 -0800

    code

--- firmware/test/limit_switch.ino
+++ firmware/test/limit_switch.ino
@@ -6,27 +6,34 @@
 //04-FEB-2018, tony - implemented pin modes and ISC 
 
 #define debug 0 // false
-#define HORIZONTAL_INTERRUPT_PIN 2
-#define LEFT_INTERRUPT_PIN 3 // Can only use 2 and 3 for Uno ISR
+#define HORIZONTAL_INTERRUPT_PIN 3
+#define LEFT_INTERRUPT_PIN 2 // Can only use 2 and 3 for Uno ISR
 #define LED_PIN 13
 
 void setup() {
+    Serial.begin(9600); // set the baud rate
+    Serial.println("Ready"); // print "Ready" once
     pinMode(LED_PIN, OUTPUT);
     pinMode(LEFT_INTERRUPT_PIN, INPUT_PULLUP);
     pinMode(HORIZONTAL_INTERRUPT_PIN, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(HORIZONTAL_INTERRUPT_PIN), stopVerticalMotors, RISING);
-    attachInterrupt(digitalPinToInterrupt(LEFT_INTERRUPT_PIN), stopLeftMotor, RISING);
+    attachInterrupt(digitalPinToInterrupt(LEFT_INTERRUPT_PIN), stopHorizontalMotor, RISING);
 }
 
 void stopHorizontalMotor()
 {
-    horizontal_stepper->stop();
+    //horizontal_stepper->stop();
+    Serial.println("works_horiz");
     return;
 }
 
 void stopVerticalMotors()
 {
-    left_vertical_stepper->stop();
-    right_vertical_stepper->stop();
+    //left_vertical_stepper->stop();
+    //right_vertical_stepper->stop();
+    Serial.println("works_vertil");
     return;
+}
+
+void loop() {    
 }
