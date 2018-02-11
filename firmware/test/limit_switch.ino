955613d50431c5ea8dae0cb39a399f9298c8ea08
Tony Singh <tonysingh1996@yahoo.com>
2018-02-10 18:36:34 -0800

    limit switch changes

--- firmware/test/limit_switch.ino
+++ firmware/test/limit_switch.ino
@@ -6,8 +6,8 @@
 //04-FEB-2018, tony - implemented pin modes and ISC 
 
 #define debug 0 // false
-#define HORIZONTAL_INTERRUPT_PIN 3
-#define LEFT_INTERRUPT_PIN 2 // Can only use 2 and 3 for Uno ISR
+#define HORIZONTAL_INTERRUPT_PIN 2
+#define LEFT_INTERRUPT_PIN 3 // Can only use 2 and 3 for Uno ISR
 #define LED_PIN 13
 
 void setup() {
@@ -16,8 +16,8 @@ void setup() {
     pinMode(LED_PIN, OUTPUT);
     pinMode(LEFT_INTERRUPT_PIN, INPUT_PULLUP);
     pinMode(HORIZONTAL_INTERRUPT_PIN, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(HORIZONTAL_INTERRUPT_PIN), stopVerticalMotors, RISING);
-    attachInterrupt(digitalPinToInterrupt(LEFT_INTERRUPT_PIN), stopHorizontalMotor, RISING);
+    attachInterrupt(digitalPinToInterrupt(HORIZONTAL_INTERRUPT_PIN), stopHorizontalMotor, RISING);
+    attachInterrupt(digitalPinToInterrupt(LEFT_INTERRUPT_PIN), stopVerticallMotors, RISING);
 }
 
 void stopHorizontalMotor()
