9f3af3d22cf165cfe9c9ff5c9fc2549961021c7c
Elvis Eshikena <elviseshikena@gmail.com>
2018-02-23 17:31:22 -0800

    Removed rouge folders and files

--- firmware/test/rembot_test/functions.ino
+++
@@ -1,9 +0,0 @@
-// @version 2.0.0
-
-void count_down() {
-    for (byte i; i<5; i++) {
-        Serial.println(i);
-    }
-    delay(100); // delay for 1/10 of a second
-    Serial.println("DONE");
-}
